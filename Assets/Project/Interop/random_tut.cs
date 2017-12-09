23c3441ebf4667637f026fe38fadbaf790a0e536
neilberard <neilberard@gmail.com>
2017-12-09 12:27:54 -0800

    Inititalizing project
    building a local repository

---
+++ Assets/Project/Interop/random_tut.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class random_tut : MonoBehaviour {
+    private int ThisInt = 0;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(ThisInt);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private int FixThis () {
+        int radius = 5;
+        return radius;
+    }
+}
